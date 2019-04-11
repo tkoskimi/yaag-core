5a5524dd9098d62c3b5450ed8cb6b3e2d852ad26
Tuomas Koskimies <tuomas.koskimies@cgi.com>
2019-04-11 14:06:42 +0300

    Minor update

--- src/defs.h
+++ src/defs.h
@@ -3,8 +3,8 @@
 // (c) Tuomas Koskimies, 2018
 
 #define OS_AMIGA 0
-#define DEBUG 1
-#define LOGGING 1
+#define DEBUG
+//#define LOGGING
 
 // The program preserves consistency in a situations where it is not necessary.
 // For example, when clearing up a list, the links can be broken up; At the end
