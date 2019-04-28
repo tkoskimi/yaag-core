f28045b1a046a2dd581529e0d776e5bafd5c24c1
Tuomas Koskimies <tuomas.koskimies@cgi.com>
2019-04-28 15:51:21 +0300

    Adds a test case for checking the limits

--- test/data_structures/quadTree.test.c
+++ test/data_structures/quadTree.test.c
@@ -82,6 +82,37 @@ static void test_index_in_custom_q(void **state) {
     assert_int_equal( 0xd, quad_point_index(q, x0, y0));
 }
 
+static void test_limits_custom_q(void **state) {
+    // Max
+    QStruct* q = quad_new_and_init( 0x0, 0x0, 16, 8);
+
+    int x0;
+    int y0;
+
+    x0 = ( 0x87 << ( q->dim - q->depth ) );
+    y0 = ( 0x74 << ( q->dim - q->depth ) );
+    assert_int_equal( 0x953a, quad_point_index(q, x0, y0));
+
+    char* name = NULL;
+    assert_string_equal( "10.01.01.01.00.11.10.10", name = quad_node_path( q, 0x953a, 0x953a ) );
+    mem_free( name );
+
+    // Min
+    q = quad_new_and_init( 0x0, 0x0, 1, 1);
+
+    x0 = ( 0x0 << ( q->dim - q->depth ) );
+    y0 = ( 0x1 << ( q->dim - q->depth ) );
+    assert_int_equal( 0x1, quad_point_index(q, x0, y0));
+
+    x0 = ( 0x1 << ( q->dim - q->depth ) );
+    y0 = ( 0x1 << ( q->dim - q->depth ) );
+    assert_int_equal( 0x3, quad_point_index(q, x0, y0));
+
+    assert_string_equal( "01", name = quad_node_path( q, 0x1, 0x1 ) );
+    assert_string_equal( "11", name = quad_node_path( q, 0x3, 0x3 ) );
+    mem_free( name );
+}
+
 static void test_node_path(void **state) {
     QStruct* q = ( ( QuadTest * ) *state )->q;
 
@@ -125,6 +156,7 @@ int quad_test() {
         cmocka_unit_test_setup_teardown( bit_masks, quad_setup, quad_teardown ),
         cmocka_unit_test_setup_teardown( test_index, quad_setup, quad_teardown ),
         cmocka_unit_test_setup_teardown( test_index_in_custom_q, quad_setup, quad_teardown ),
+        cmocka_unit_test_setup_teardown( test_limits_custom_q, quad_setup, quad_teardown ),
         cmocka_unit_test_setup_teardown( test_node_path, quad_setup, quad_teardown ),
         cmocka_unit_test_setup_teardown( test_node_path_in_custom_q, quad_setup, quad_teardown ),
     };
