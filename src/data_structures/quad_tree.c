38424339027131cab6d85881173486479191882d
Tuomas Koskimies <tuomas.koskimies@cgi.com>
2019-04-28 16:22:03 +0300

    Updates the doc

--- src/data_structures/quad_tree.c
+++ src/data_structures/quad_tree.c
@@ -16,12 +16,12 @@
 // Note. The names must not be longer than LEVEL_NAME_MAX_LENGTH (see tree.h).
 char* QUAD_PATHS[] = { "00.", "01.", "10.", "11." };
 
-inline unsigned int _bit_mask_011(unsigned int x) {
-    return (x >= sizeof(unsigned) * CHAR_BIT) ? (unsigned) -1 : (1U << x) - 1;
+inline unsigned int _bit_mask_011(unsigned int n) {
+    return (n >= sizeof(unsigned) * CHAR_BIT) ? (unsigned) -1 : (1U << n) - 1;
 }
 
-inline unsigned int _bit_mask_010(unsigned int x, unsigned int y) {
-    return ( x == y ) ? 0 : (((unsigned) -1 >> (32 - (y))) & ~((1U << (x)) - 1));
+inline unsigned int _bit_mask_010(unsigned int m, unsigned int n) {
+    return ( m == n ) ? 0 : (((unsigned) -1 >> (32 - (n))) & ~((1U << (m)) - 1));
 } 
 
 QStruct* quad_new() {
@@ -155,76 +155,4 @@ char* quad_node_path( QStruct* q, int index_tl, int index_br ) {
  * Q3: x0 <= x < x1, y1 <= y < y2
  * Q2: x1 <= x < x2, y1 <= y < y2
  *
- * Question: Which subquadrant containts the box? 
  */
-// QNode* quad_insert_rcrsv( QArray* tree, void* new_data, int x0, int y0, int x1, int y1 ) {
-//     return _quad_insert_rcrsv( NULL, new_data, x0, y0, x1, y1, tree->x, tree->y, tree->w, tree->h, tree->d );
-// }
-
-// QNode* _quad_insert_rcrsv( QNode* node, void* new_data, int x0, int y0, int x1, int y1, int tl_x, int tl_y, int br_x, int br_y, int d ) {
-//     assert( node && QUAD_TREE_NULL );
-//     assert( ( x0 < x1 ) && ( y0 < y1 ) && QUAD_BOX_POS_ORIENTATION );
-//     assert( ( x1 - x0 <= br_x - tl_x ) && ( y1 - y0 <= br_y - tl_y ) && QUAD_TOO_BIG );
-
-//     // The middle point
-//     int mp_x = tl_x + ( br_x - tl_x )/2; // Ex. 10 + (13 - 10)/2 = 11
-//     int mp_y = tl_y + ( br_y - tl_y )/2;
-//     // qn indicates the quadrant of pn
-//     int q0 = _get_quad( mp_x, mp_y, x0, y0 );
-//     int q1 = _get_quad( mp_x, mp_y, x1, y1 );
-
-//     // If the quadrants of p0 and p1 are different, or if the size of the
-//     // quadrant is less or equal than the minimum, then the data is stored
-//     // in this node.
-//     if ( ( q0 != q1 ) || ( mp_x - tl_x <= d ) || ( mp_y - tl_y <= d ) ) {
-//         // if ( node->boxes ) {
-//         //     // Todo: Change the type of the boxe
-//         //     dbllist_push( node->boxes[0], new_data );
-//         // }
-//     } else {
-//         if ( q0 == 0 ) {
-//             if ( node->tl == NULL ) {
-//                 node->tl = new_qnode();
-//             }
-//             _quad_insert_rcrsv( node->tl, new_data, x0, y0, x1, y1, tl_x, tl_y, mp_x, mp_y, d );
-//         } else if ( q0 == 1 ) {
-//             if ( node->tr == NULL ) {
-//                 node->tr = new_qnode();
-//             }
-//             _quad_insert_rcrsv( node->tr, new_data, x0, y0, x1, y1, mp_x + 1, tl_y, br_x, mp_y, d);
-//         } else if ( q0 == 2 ) {
-//             if ( node->bl == NULL ) {
-//                 node->bl = new_qnode();
-//             }
-//             _quad_insert_rcrsv( node->bl, new_data, x0, y0, x1, y1, tl_x, mp_y + 1, mp_x, br_y, d );
-//         } else {
-//             if ( node->br == NULL ) {
-//                 node->br = new_qnode();
-//             }
-//             _quad_insert_rcrsv( node->br, new_data, x0, y0, x1, y1, mp_x + 1, mp_y + 1, br_x, br_y, d);
-//         }
-//     }
-// }
-
-// QNode* new_qnode() {
-//     QNode* node = ( QNode* ) mem_malloc( sizeof( QNode ) );
-//     //node->boxes = dbllist_new();
-//     node->tl = NULL;
-//     node->tr = NULL;
-//     node->bl = NULL;
-//     node->bl = NULL;
-//     return node;
-// }
-
-// int _get_quad( int mp_x, int mp_y, int x, int y ) {
-//     int q = 0;
-//     if ( x > mp_x ) q += 1;
-//     if ( y > mp_y ) q += 2;
-//     return q;
-// }
-
-// void quad_del_subtree() {
-// }
-
-// void* quad_del( QArray *tree, void* data ) {
-// }
--- src/data_structures/quad_tree.h
+++ src/data_structures/quad_tree.h
@@ -39,43 +39,74 @@ typedef struct {
     unsigned int depth;
 } QStruct;
 
-unsigned int _bit_mask_011(unsigned int x);
+// Returns a bit mask whose first n bits are 1s
+//
+// @param n The index of the first bit that is set to 1.
+// @return The bit mask. For example, if n is 5 the return value is b00011111.
+// @see https://stackoverflow.com/questions/8774567/
+//      c-macro-to-create-a-bit-mask-possible-and-have-i-found-a-gcc-bug
+unsigned int _bit_mask_011(unsigned int n);
 
-unsigned int _bit_mask_010(unsigned int x, unsigned int y);
+// Returns a bit mask whose bits in the range (m+1,n) are 1s; Others are 0s
+//
+// @param m The index of the last 0 before 1s.
+// @param n The index of the last 1.
+// @return The bit mask. For example, if m is 2 and n is 5 the return value is
+//      b00011100.
+// @see https://stackoverflow.com/questions/8774567/
+//      c-macro-to-create-a-bit-mask-possible-and-have-i-found-a-gcc-bug
+unsigned int _bit_mask_010(unsigned int m, unsigned int n);
 
-// Creates a new array of QNodes
 QStruct* quad_new();
 
-// Creates a new array of QNodes
 QStruct* quad_new_and_init(
     unsigned int x0,
     unsigned int y0,
     unsigned int dim_in_bits,
     unsigned int depth_of_qtree );
 
-// Releases the array
 void quad_free( QStruct *q );
 
+// Returns an index of a quadrants that the point is belonging to
+//
+// @param q The pointer to a quad structure.
+// @param x0 The x coordinate of the point.
+// @param y0 The y coordinate of the point.
+// @return The index of the point. The value is explained below:
+//
+// The coordinates are shifted to start from the beginning of a memory unit.
+// Hence, we are interested in coordinates that have following format:
+//
+// MSB                                         1:st bit
+//                          <------ dim_in_bits ------>
+// +-----+-----+--...-+-----+-----+-------+--...+-----+
+// |  0  |  0  |  ... |  0  |x0s.n|x0s.n-1|  ...+x0s.1|
+// +-----+-----+--...-+-----+-----+-------+--...+-----+
+//
+// The last bits, 1 - n, define the modulo 2^( dim_in_bits - depth ), the
+// bits 2 - n modulo 2^( dim_in_bits - depth + 1 ) and so on. These modulos
+// build up a hierarchy of the quadrants in one dimension; In two
+// dimensions, we need both coordinates, x and y.
+//
+// Two bits, e.g., x0s.n and y0s.n define the ( depth_of_qtree - n + 1 ):th
+// level of quadrants. Hence, we pack the index in the following way:
+//
+// MSB                                                             1:st bit
+//    +-----+--...+-----+---------+---------+--...+-----+-----+-----+-----+
+//    |  0  |--...|  0  |x0s.depth|y0s.depth|  ...|x0s.2|y0s.2|x0s.1|y0s.1|
+//    +-----+--...+-----+---------+---------+--...+-----+-----+-----+-----+
+//
+// For example, if x0s == 0x2 and y0s == 0x3, the index will be b1101 == 0xd.
+// @see Test cases
 int quad_point_index( QStruct* qstruct, unsigned int x0, unsigned int y0 );
 
+// Returns a path of the quadrant that contains both points, tl and br
+//
+// @param q The pointer to a quad structure.
+// @param index_tl The index of the quadrant that contains the tl.
+// @param index_br The index of the quadrant that contains the br.
+// @return The path, e.g. "00.01"; NULL if there is no quadrant that contains the
+//      tl and/or br.
 char* quad_node_path( QStruct* q, int index_tl, int index_br );
-
-// Inserts a new node to the array
-// QNode* quad_insert_rcrsv( QArray* tree, void* new_data, int x0, int y0, int x1, int y1 );
-
-// QNode* _quad_insert_rcrsv( QNode* node, void* new_data, int x0, int y0, int x1, int y1, int tl_x, int tl_y, int br_x, int br_y, int d );
-
-// QNode* new_qnode();
-
-// int _get_quad( int mp_x, int mp_y, int x, int y );
-
-// void quad_del_subtree();
-
-// void* quad_del( QArray *tree, void* data );
-
-// // Deletes the node from the array
-
-// // @return The index of the array of the game object
-// int quad_index( int x0, int y0, int x1, int y1 );
 
 #endif // _quadtree_
