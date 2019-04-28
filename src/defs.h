6d7fea13945ab5db967693bc5771440b3588aa27
Tuomas Koskimies <tuomas.koskimies@cgi.com>
2019-04-28 14:20:11 +0300

    Adds COORDINATE_SIZE_IN_BITS

--- src/defs.h
+++ src/defs.h
@@ -6,6 +6,9 @@
 #define DEBUG
 //#define LOGGING
 
+// The size of the coordinates in bits.
+#define COORDINATE_SIZE_IN_BITS 32
+
 // The program preserves consistency in a situations where it is not necessary.
 // For example, when clearing up a list, the links can be broken up; At the end
 // the list will be empty.
